06f2af5690d00b1567f0654b6dde062a84abf94d
SaidSultan24 <124450122+SaidSultan2@users.noreply.github.com>
2023-05-15 06:38:16 +0500

    created simple counter

--- js/main.js
+++ js/main.js
@@ -23,3 +23,30 @@ closeBtn.addEventListener('click', ()=> {
     menuOpen=false
 
 })
+
+
+// Counter
+
+let section = document.querySelector(".counter-container");
+let stat = document.querySelectorAll(".stats .sta");
+let start = false;
+
+window.onscroll = function() {
+    if (window.scrollY >= section.offsetTop) {
+        if (!start) {
+            stat.forEach((sta) => startCount(sta));
+        }
+        start = true
+    }
+}
+
+function startCount(el) {
+    let max = el.dataset.max;
+
+    let count = setInterval(()=>{
+        el.textContent++;
+        if(el.textContent == max) {
+            clearInterval(count);
+        }
+    },2000/stat)
+}
